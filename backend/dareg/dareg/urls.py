7d74d2962c938f03b65562c085cdae76b8a9c35a
Tomáš Svoboda <323969@muni.cz>
2023-10-25 22:29:05 +0200

    addition of urls

--- backend/dareg/dareg/urls.py
+++ backend/dareg/dareg/urls.py
@@ -8,6 +8,8 @@ router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 router.register(r"facilities", views.FacilityViewSet)
+router.register(r"projects", views.ProjectViewSet)
+router.register(r"datasets", views.DatasetViewSet)
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
