08acca62e2a8e206f0811c82876b0c44a2861e1e
Tomáš Svoboda <323969@muni.cz>
2023-10-25 22:42:09 +0200

    add entities to admin interface

--- backend/dareg/api/admin.py
+++ backend/dareg/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Facility, Project, Dataset, Template, FilledTemplate, Language, UserProfile
 
-# Register your models here.
+admin.site.register(Facility)
+admin.site.register(Project)
+admin.site.register(Dataset)
+admin.site.register(Template)
+admin.site.register(FilledTemplate)
+admin.site.register(Language)
+admin.site.register(UserProfile)
