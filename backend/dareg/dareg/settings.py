a19c0cf3024cbcfe0356b789b493978e1a324509
Tomáš Svoboda <323969@muni.cz>
2023-11-01 11:56:02 +0100

    Refactor env variables

--- backend/dareg/dareg/settings.py
+++ backend/dareg/dareg/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ["DJANGO_DEBUG"])
+DEBUG = eval(os.environ["DJANGO_DEBUG"].title())
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split()
 
 # Application definition
 
